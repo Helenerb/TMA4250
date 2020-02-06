10b58b597980b0fcbecf9c1a372591f56729a202
Helene Behrens <helenerandi@gmail.com>
2020-02-06 19:50:46 +0100

    faktisk plotting 1a

--- Problem1.R
+++ Problem1.R
@@ -26,3 +26,37 @@ corr.mat3 = Matern(d,range=10,nu=nu.mat[2],phi=1)
 lines(d/10,corr.mat1,type="l",col="red")  # usikker på om vi skal plotte som funk av d eller d/10 = tau?
 lines(d/10,corr.mat3,type="l",col="green")
 
+#Related variograms:
+sigma <- c(1,5)
+
+var.exp1.1 <- sigma[1]*(1-corr.exp1)
+var.exp1.5 <- sigma[2]*(1-corr.exp1)
+
+var.exp1.9.1 <- sigma[1]*(1-corr.exp1.9)
+var.exp1.9.5 <- sigma[2]*(1-corr.exp1.9)
+
+var.mat1.1 <- sigma[1]*(1-corr.mat1)
+var.mat1.5 <- sigma[2]*(1-corr.mat1)
+
+var.mat3.1 <- sigma[1]*(1-corr.mat3)
+var.mat3.5 <- sigma[2]*(1-corr.mat3)
+
+#Plot correlation function with corresponding variograms, variance {1,5}
+
+plot(d/10,var.exp1.5,type="l")
+lines(d/10,var.exp1.1,type="l",col="orange")
+lines(d/10,corr.exp.1,type="l",col="blue")
+
+plot(d/10,var.exp1.9.5,type="l")
+lines(d/10,var.exp1.9.1,type="l",col="orange")
+lines(d/10,corr.exp1.9,type="l",col="blue")
+
+plot(d/10,var.mat1.5,type="l")
+lines(d/10,var.mat1.1,type="l",col="orange")
+lines(d/10,corr.mat1,type="l",col="blue")
+
+plot(d/10,var.mat3.5,type="l")
+lines(d/10,var.mat3.1,type="l",col="orange")
+lines(d/10,corr.mat3,type="l",col="blue")
+
+
