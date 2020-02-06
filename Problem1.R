2e965b40b1e35f9d7e4943aa4ae2c662342ba4c1
Helene Behrens <helenerandi@gmail.com>
2020-02-06 21:55:11 +0100

    plot realisations of prior

--- Problem1.R
+++ Problem1.R
@@ -2,6 +2,7 @@ library(fields);
 library(geoR);  
 library(MASS)
 library(akima)
+library(mvtnorm)
 
 d <- 0:49    # All possible distances 
 phi <- 10    # Usikker på denne, men tror det kommer av def av rho i oppgaven 
@@ -59,4 +60,18 @@ plot(d/10,var.mat3.5,type="l")
 lines(d/10,var.mat3.1,type="l",col="orange")
 lines(d/10,corr.mat3,type="l",col="blue")
 
+cov.vec <- cov.spatial(0:49,cov.model="matern",cov.pars=c(sigma[1],phi),kappa=nu.mat[1])
+
+cov.matrix <- matrix(data=0,nrow=50,ncol=50)
+for(i in 1:50){
+  for(j in 1:50){
+    cov.matrix[i,j] = cov.vec[abs(i-j)+1]
+  }
+}
+
+prior.real <- rmvnorm(4,rep(0,50),cov.matrix)
+plot(d,prior.real[4,],type="l",col="orange")
+lines(d,prior.real[1,],type="l",col="red")
+lines(d,prior.real[2,],col="green")
+lines(d,prior.real[3,],col="blue")
 
