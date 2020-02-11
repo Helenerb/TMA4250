20fe6ce0ce240ff3c3c4c8638b6e1639da06b4f3
Helene Behrens <helenerandi@gmail.com>
2020-02-11 17:08:08 +0100

    problem 2c

---
+++ Problem2.R
@@ -0,0 +1,31 @@
+library(fields); 
+library(geoR);  
+library(MASS)
+#install.packages("akima")
+library(akima)
+library(ggplot2)
+
+topo <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise1/topo.dat")
+topo.geodata <- list("coords"=cbind(topo$x,topo$y),"data"=cbind(topo$z))
+pred.points <- expand.grid(seq(1,315,by=1),seq(1,315,by=1))
+
+krige.params <- krige.control(type.krige="ok",trend.d="cte",trend.l = "cte",cov.model="powered.exponential",cov.pars=c(2500,100),kappa=1.5)
+krige.pred <- krige.conv(coords = topo.geodata$coords,data=topo.geodata$data,locations=pred.points,krige=krige.params)
+
+##Plot: 
+
+# make krige.pred into dataframe for easier plotting
+krige.df <- data.frame("x"=pred.points$Var1,"y"=pred.points$Var2,"z"=krige.pred$predict, "v"=krige.pred$krige.var, "obs.x"=topo$x, "obs.y"=topo$y, "obs.z"=topo$z)
+
+# plotting the Krige prediction 
+krige.plot = ggplot() + geom_tile(data=krige.df, aes(x=x, y=y, fill=z)) + scale_fill_gradientn(colors=rainbow(4))
+krige.plot = krige.plot + theme_classic()
+krige.plot
+
+# plotting the variance from the Krige prediction
+krige.plot.var = ggplot() + geom_tile(data=krige.df, aes(x=x, y=y, fill=v)) + scale_fill_gradientn(colors=cm.colors(7))
+krige.plot.var = krige.plot.var + theme_classic()
+krige.plot.var = krige.plot.var + geom_point(data=topo, aes(x=topo$x,y=topo$y),shape=4)
+krige.plot.var
+
+
