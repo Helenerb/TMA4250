31578e2fd98b05c0d5e86ef870c7feac53a42b8f
Helene Randi Behrens <helenerb@banach05.math.ntnu.no>
2020-02-17 18:20:46 +0100

    ryddet opp i 3

--- task3-fredag.R
+++ task3-fredag.R
@@ -4,12 +4,12 @@ library(mvtnorm)
 library(ggplot2)
 #a)
 
-set.seed(125)
-n=30
+set.seed(124)
+n=30  # grid size
 L=seq(1:n)
 L.grid=expand.grid(L,L)
 
-var_r = 2; ksi_r = 3
+var_r = 2; ksi_r = 3  # global variables for marameters
 cov.mat=varcov.spatial(coords=L.grid,dists.lowertri=NULL,cov.model="powered.exponential", kappa=1, cov.pars=c(var_r,3))
 realizations.RF=as.vector(rmvnorm(n=1,mean=numeric(n*n),cov.mat$varcov,method="chol"))
 
@@ -21,50 +21,36 @@ vgram.sampled=variog(coords=L.grid,data=df$z,uvec=13)
 vgram.analytic =var_r*(1-exp(-vgram.sampled$u/ksi_r))
 vgram = data.frame("sampled"=vgram.sampled$v, "analytic"=vgram.analytic, "dist"=vgram.sampled$u)
 vgram <- reshape2::melt(vgram, id.var="dist")
-ggplot(data=vgram, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic()
+ggplot(data=vgram, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic() + ggtitle(label="seed = 124")
+
+# c): change seed to 124 and 125 and run lines 20-24, from task b)
 
 # d
 
-#vgram.sampled=variog(coords=L.grid,data=df$z,uvec=13)
-vgram.analytic =var_r*(1-exp(-vgram.sampled$u/ksi_r))
-#vgram = data.frame("sampled"=vgram.sampled$v, "analytic"=vgram.analytic, "dist"=vgram.sampled$u)
-#vgram <- reshape2::melt(vgram, id.var="dist")
-#ggplot(data=vgram, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic() + ggtitle("seed = 125")
-
 # sample 36 locations
-sample.idx <- sort(as.integer(runif(36,1,900)))
+sample.idx <- sort(sample.int(900,36))
 sample.locs <- L.grid[sample.idx,]
-sample.reals <- df[sample.idx,]
+sample.reals <- df[sample.idx,]  # get realization at sampled points
 
-#vgram.sampled.36 = variog(coords=sample.locs, data=sample.reals$z, uvec=13)
-#vgram.analytic.36 = var_r*(1-exp(-vgram.sampled.36$u/ksi_r))
-#vgram.36 = data.frame("sampled"=vgram.sampled.36$v, "analytic"=vgram.analytic, "dist"=vgram.sampled.36$u)
-#vgram.36 <- reshape2::melt(vgram.36, id.var="dist")
-#ggplot(data=vgram.36, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic() + ggtitle("From 36 sampled values")
+vgram.sampled.36 = variog(coords=sample.locs, data=sample.reals$z, uvec=13)  # creating variogram from 36 samples
+vgram.analytic.36 = var_r*(1-exp(-vgram.sampled.36$u/ksi_r))
+vgram.36 <- data.frame("sampled"=vgram.sampled.36$v, "analytic"=vgram.analytic, "dist"=vgram.sampled.36$u)
+vgram.36 <- reshape2::melt(vgram.36, id.var="dist")
+ggplot(data=vgram.36, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic() + ggtitle("From 36 sampled values")
 
 # considering sigma squared and xi to be unknown:
 
-fitted.params = likfit(coords=L.grid, data=df$z, ini.cov.pars = c(2,1))
-fitted.params
-vgram.fitted = fitted.params$sigmasq*(1- exp(-seq(0:40)/fitted.params$phi))
-
-vgram.analytic.d =var_r*(1-exp(-seq(0:40)/ksi_r))
-
-vgram.d = data.frame("fitted"=vgram.fitted, "analytic"=vgram.analytic.d, "dist"=seq(0:40))
-vgram.d <- reshape2::melt(vgram.d, id.var="dist")
-ggplot(data=vgram.d, aes(x=dist, y=value, col=variable)) + geom_line() + theme_classic() + ggtitle("All observations used for ML estimation, estimated variance = 2.40 and estimated range = 3.91")
-
 ML.variograms <- function(n,ini.cov.pars,title,L.grid,df,var_r,ksi_r){
-  sample.idx <- sort(sample.int(900,n))
+  sample.idx <- sort(sample.int(900,n))  # sample n random locations
   sample.locs <- L.grid[sample.idx,]
   sample.reals <- df[sample.idx,]
   
-  vgram.analytic.d =var_r*(1-exp(-seq(0:40)/ksi_r))
+  vgram.analytic.d =var_r*(1-exp(-seq(0:40)/ksi_r))  #defining the analytic variogram
   
   fitted.params = likfit(coords=sample.locs, data=sample.reals$z, ini.cov.pars = ini.cov.pars)
   fitted.params
   
-  vgram.fitted = fitted.params$sigmasq*(1- exp(-seq(0:40)/fitted.params$phi))
+  vgram.fitted = fitted.params$sigmasq*(1- exp(-seq(0:40)/fitted.params$phi))  # using the analytical expression with fitted parameters to make variogram
   
   vgram.d = data.frame("fitted"=vgram.fitted, "analytic"=vgram.analytic.d, "dist"=seq(0:40))
   vgram.d <- reshape2::melt(vgram.d, id.var="dist")
@@ -74,15 +60,17 @@ ML.variograms <- function(n,ini.cov.pars,title,L.grid,df,var_r,ksi_r){
 
 require(gridExtra)
 
-
+# plots for 3d)
 plot9 = ML.variograms(9,c(2,1),"Using 9 observations",L.grid,df,var_r,ksi_r)
 plot64 = ML.variograms(64,c(2,1),"Using 64 observations",L.grid,df,var_r,ksi_r)
 plot100 = ML.variograms(100,c(2,1),"Using 100 observations",L.grid,df,var_r,ksi_r)
 
-#grid.arrange(plot9,plot64,plot100,ncol=3)
+grid.arrange(plot9,plot64,plot100,ncol=3)
 
+
+# plots for 3e)
 plotall = ML.variograms(900,c(2,1),"Using all observations",L.grid,df,var_r,ksi_r)
-plot36 = ML.variograms(36,c(2,1),"Using 9 observations",L.grid,df,var_r,ksi_r)
+plot36 = ML.variograms(36,c(2,1),"Using 36 observations",L.grid,df,var_r,ksi_r)
 
 grid.arrange(plotall,plot36,ncol=2)
 
