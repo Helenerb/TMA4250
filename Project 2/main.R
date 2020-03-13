ca57a85f4776d975c7708ae3db34286ae8ab3a8a
Helene Behrens <helenerandi@gmail.com>
2020-03-13 15:39:01 +0100

    laster inn data

--- Project 2/main.R
+++ Project 2/main.R
@@ -1,1 +1,10 @@
 # main
+library(spatial)
+library(MASS)
+
+cells <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/cells.dat")
+redwood <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/redwood.dat")
+#TODO: VOJIN! Endre filen her til der den ligger hos deg! 
+pines <- read.table("C://Users//helen//Documents//GitHub//Spatial Statistics//TMA4250//Project 2//pines.txt")
+
+
