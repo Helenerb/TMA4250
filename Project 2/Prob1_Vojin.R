f80692ce26ae54cb69292eef1fb979a110149299
Helene Behrens <helenerandi@gmail.com>
2020-03-16 14:32:15 +0100

    J.hat funker kanskje

--- Project 2/Prob1_Vojin.R
+++ Project 2/Prob1_Vojin.R
@@ -6,9 +6,9 @@ library(reshape2)
 library(RColorBrewer)
 
 #read data
-cells   = read.table(file = 'cells.dat.txt', col.names = c('x', 'y'))
-pines   = read.table(file = 'pines.dat.txt', skip=3, col.names = c('x', 'y')) #first 3 lines not important?
-redwood = read.table(file = 'redwood.dat.txt', col.names = c('x', 'y'))
+cells   = read.table(file = 'cells.dat', col.names = c('x', 'y'))
+pines   = read.table(file = 'pines.txt', col.names = c('x', 'y')) #first 3 lines not important?
+redwood = read.table(file = 'redwood.dat', col.names = c('x', 'y'))
 
 #initial plots
 ggplot(data = cells, aes(x=x, y=y)) + 
--- Project 2/Problem1.R
+++ Project 2/Problem1.R
@@ -2,44 +2,86 @@
 library(spatial)
 library(MASS)
 library(ggplot2)
+library(gridExtra)
+library(reshape2)
+library(RColorBrewer)
+library(datasets)
 
-cells <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/cells.dat")
-redwood <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/redwood.dat")
+cells <- read.table("cells.dat.txt", col.names=c('x', 'y'))
+redwood <- read.table("redwood.dat.txt", col.names=c('x',  'y'))
 #TODO: VOJIN! Endre filen her til der den ligger hos deg! 
-pines <- read.table("C://Users//helen//Documents//GitHub//Spatial Statistics//TMA4250//Project 2//pines.txt")
+pines <- read.table(file = 'pines.dat.txt', skip=3, col.names = c('x','y'))
 
 # a)
 # Visualize plots:
 
 # Cells:
-cells.plot <- ggplot(data=cells, aes(x=V1, y=V2)) + geom_point(color="darkolivegreen4") +
+cells.plot <- ggplot(data=cells, aes(x=x, y=y)) + geom_point(color="darkolivegreen4") +
   xlab(label=" ") + ylab(label=" ") + ggtitle(label="Cells")
 cells.plot
 
 # Redwood:
-redwood.plot <- ggplot(data=redwood, aes(x=V1, y=V2)) + geom_point(color="sienna") +
+redwood.plot <- ggplot(data=redwood, aes(x=x, y=y)) + geom_point(color="sienna") +
   xlab(label=" ") + ylab(label=" ") + ggtitle(label="Redwood")
 redwood.plot
 
 # Pines:
-pines.plot <- ggplot(data=pines, aes(x=V1, y=V2)) + geom_point(color="springgreen4") +
+pines.plot <- ggplot(data=pines, aes(x=x, y=y)) + geom_point(color="springgreen4") +
   xlab(label=" ") + ylab(label=" ") + ggtitle(label="Pines")
 pines.plot
 
 
 ## b)
-pp.cells <- ppinit("cells.dat")
-pp.pines <- ppinit("pines.dat")
-pp.redwood <- ppinit("redwood.dat")
 
-L.cells <- Kfn(pp.cells, 1)
-L.pines <- Kfn(pp.pines, sqrt(2))
-L.redwood <- Kfn(pp.redwood, sqrt(2))
+ppregion()
+L.cells <- Kfn(cells, fs = sqrt(2))
+L.pines <- Kfn(pines, fs = sqrt(2))
+L.redwood <- Kfn(redwood, fs = sqrt(2))
 
-as.data.frame(L.cells)
+plot(L.redwood, type="b")
+plot(L.pines,type="b")
+plot(L.cells, type="b")
 
-L.plot <- ggplot() + geom_point(data=as.data.frame(L.cells),  aes(x=x, y=y), color="darkolivegreen4")
+## c)
 
-read.table("towns.dat")
+# implementerer simuleringsalgoritme:
+
+sim.stat.poiss <- function(no){
+  x <- runif(no)
+  y <- runif(no)
+  df<- data.frame(x, y)
+  return(df)
+}
+
+J.hat <- function(df, t.end, no){
+  ts = seq(0.01,t.end,by=0.01)
+  result = rep(0,t.end)
+  for(k in 1:length(ts)){
+    t <- ts[k]
+    norm <- no*(pi*t^2)          # area of Ball with radius t with arbitrary center x. 
+    # ^Not accounted for boundary
+    sum.outer <- 0
+    for( i in 1:no){
+      x0 <- df$x[i]
+      y0 <- df$y[i]
+      sum.inner <- 0
+      for (j in 1:no){
+        if( sqrt((x0 - df$x[j])^2 + (y0 - df$y[j])^2) <= t){
+          sum.inner <- sum.inner + 1
+        }
+      }
+      sum.outer = sum.outer + sum.inner - 1
+    }
+    result[k] <- sum.outer/norm
+  }
+  return(data.frame(result))
+}
+
+test.df <- sim.stat.poiss(10)
+result.test <- J.hat(test.df,0.7,10)
+
+lambda.hat <- function(df){
+  
+}
 
 
