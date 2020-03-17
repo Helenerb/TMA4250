d2be87db54409fc3a8cd5c2f5390173e15754f26
Helene Behrens <helenerandi@gmail.com>
2020-03-17 16:57:48 +0100

    Neumann-Scott funker sånn tålelig, torus-2d mangler

--- Project 2/Problem3.R
+++ Project 2/Problem3.R
@@ -7,6 +7,7 @@ library(gridExtra)
 library(reshape2)
 library(RColorBrewer)
 library(datasets)
+library(MASS)
 
 redwood <- read.table("redwood.dat.txt", col.names=c('x',  'y'))
 
@@ -19,8 +20,60 @@ L.redwood <- Kfn(redwood, fs = sqrt(2))
 
 # Simulation algorithm for Neumann-Scott 
 
+# torus.2d help function:
+
+torus.2d <- function(x){
+  return(x)
+}
+
 
 NS <- function(lamb.M, sigma.c, p.mu, p.sigma){
   k <- 0
-  k.M <- rpois(1,lamb.M)
+  k.M <- rpois(1,lamb.M)                    # sample no of mother points
+  
+  # initiate matrix to hold child events 
+  x.C.all <- matrix(0L, nrow = 0, ncol=2)
+  colnames(x.C.all) <- c("x", "y")
+  
+  #initiate matrix to hold mother events
+  x.mother <- matrix(0L, nrow = 0, ncol=2)
+  colnames(x.mother) <- c("x", "y")
+  
+  for(j in 1:k.M){
+    # sample location of mother node
+    x.M <- runif(1)
+    y.M <- runif(1)
+    x.mother <- rbind(x.mother, c(x.M, y.M)) # store mother event
+    
+    k.C <- rnorm(1,mean = p.mu, sd=sqrt(p.sigma))             # sample no of child points 
+    
+    # sample k.C child points
+    for(i in 1:k.C){
+      cat(k.C, "\n")
+      cat(c(x.M, y.M), "\n")
+      cat(sigma.c*diag(2), "\n")
+      x.C <- mvrnorm(1,c(x.M, y.M),sigma.c*diag(2))
+      cat(x.C, "\n")
+      
+      # transform to torus representation of D
+      x.C <- torus.2d(x.C)
+      
+      x.C.all <- rbind(x.C.all, x.C)
+    }
+       # store child events
+    cat(x.C.all, "\n")
+  }
+  return(list("x.C.all" = x.C.all, "x.mother"=x.mother))
 }
+
+test <- NS(7, 0.125^2, 8, 10)
+test$x.C.all
+x <- test$x.C.all[,"x"]
+y <- test$x.C.all[,"y"]
+
+x.M <- test$x.mother[,1]
+y.M <- test$x.mother[,2]
+
+plot(x,y, col="red")
+points(x.M,y.M, col="green")
+test.df <- data.frame("x"=x,"y"=y)
